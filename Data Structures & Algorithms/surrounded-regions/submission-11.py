bdb39432d3f78d5f893af2ae36dc5964c24f0abc
Emilien Valat <46785587+Emvlt@users.noreply.github.com>
2026-05-05 10:32:54 +0200

    Add: surrounded-regions - submission-11

---
+++ Data Structures & Algorithms/surrounded-regions/submission-11.py
@@ -0,0 +1,44 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows = len(board)
+        cols = len(board[0])
+
+        connected = set()
+
+        def getNeighbours(i,j):
+            for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
+                ni, nj = i+di, j+dj
+                if 0 <= ni < rows and 0 <= nj < cols: 
+                    yield ni, nj
+
+        def search(i,j):
+            if board[i][j] == 'X':
+                return 
+
+            if (i,j) in connected:
+                return
+
+            queue = deque([(i,j)])
+            while queue:
+                length = len(queue)
+                for _ in range(length):
+                    i,j = queue.popleft()
+
+                    connected.add((i,j))
+                    for ni, nj in getNeighbours(i,j):
+                        if board[ni][nj] != 'X' and (ni, nj) not in connected:
+                            queue.append((ni,nj))
+
+        for i in range(rows):
+            
+            search(i, 0)
+            search(i, cols-1)
+
+        for j in range(cols):
+            search(0, j)
+            search(rows-1, j)
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'O' and (i,j) not in connected:
+                    board[i][j] = 'X'
