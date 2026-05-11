89cc97aa131727172d8a277002d1a53c703a9420
Emilien Valat <46785587+Emvlt@users.noreply.github.com>
2026-05-11 09:10:06 +0200

    Add: combination-target-sum-ii - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        solutions = []
+
+        # Inplace sorting of the small list
+        candidates.sort()
+
+        def backtracking(i, sol, current):
+            if current > target:
+                return
+
+            if current == target:
+                solutions.append(sol[:])
+                return
+
+            for j in range(i, n):
+                if candidates[j]>target:
+                    return
+                    
+                if j > 0 and j>i and candidates[j] == candidates[j-1]:
+                    continue
+                else:
+                    sol.append(candidates[j])
+                    backtracking(j+1, sol, current+candidates[j])
+                    sol.pop()
+
+        backtracking(0, [], current=0)
+        return solutions
