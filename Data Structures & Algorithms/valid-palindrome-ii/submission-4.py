8c7d6cf160b29617e05816dcf61f74987406ead3
Emilien Valat <46785587+Emvlt@users.noreply.github.com>
2026-06-12 09:26:22 +0200

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        cleaned = [c.lower() for c in s if c.isalnum()]
+    
+        l = 0
+        r = len(cleaned) - 1
+        
+        while l < r:                
+            if cleaned[l] != cleaned[r]:
+                return cleaned[l+1: r+1]==cleaned[l+1: r+1][::-1] or cleaned[l: r]==cleaned[l:r][::-1]
+
+            l += 1
+            r -= 1
+
+        return True 
